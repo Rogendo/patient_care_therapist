dfcfbfcb4cdb9cb5789045aba64a4e328b3ee325
Rogendo <peterrogendo1999@gmail.com>
2024-10-30 01:32:44 +0300

    removed the sessionId

--- src/utils/FlowiseAI.tsx
+++ src/utils/FlowiseAI.tsx
@@ -1,15 +1,12 @@
 import axios from 'axios';
 
-async function query(data: any, sessionId: string): Promise<any> {
-    // Make sure to pass the sessionId in each request
-    const overrideConfig = {
-        "sessionId": sessionId,
-    };
-    const apiUrl = process.env.REACT_APP_API_URL; // Get API URL from env
-    const chatId = process.env.CHAT_ID; // Get CHAT_ID from env
+async function query(data: any): Promise<any> {
+//    const apiUrl = process.env.REACT_APP_API_URL; // Get API URL from env
+//    const chatId = process.env.CHAT_ID; // Get CHAT_ID from env
 
     // Sending the chat request
-    const response = await axios.post(`${apiUrl}/api/v1/prediction/${chatId}/`, data, {
+    // const response = await axios.post(`${apiUrl}/api/v1/prediction/${chatId}/`, data, {
+        const response = await axios.post("https://server.chat.patientcare.care/api/v1/prediction/238415e6-0cda-458f-8510-ec9aeda447f4/", data, {
         headers: {
             "Authorization": "Bearer JWT",
             "Content-Type": "application/json",
@@ -17,15 +14,8 @@ async function query(data: any, sessionId: string): Promise<any> {
     });
 
     console.log('Response data:', response.data);
-    console.log(`Received sessionId: ${sessionId} for question: ${data}`);
-
-    // Updating the session in Flowise
-    await axios.post('http://localhost:3000/api/v1/session', overrideConfig, {
-        headers: {
-            "Authorization": "Bearer JWT",
-            "Content-Type": "application/json"
-        }
-    });
+   // console.log("Received sessionId: ${sessionId} for question: ${data}");
+    console.log("This is test");
 
     return response.data;
 }
